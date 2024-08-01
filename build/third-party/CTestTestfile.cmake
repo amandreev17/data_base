5c00362eeb98ea37352f23b1ef8acf65234ae059
amandreev17 <144845041+amandreev17@users.noreply.github.com>
2024-08-01 11:01:09 +0300

    Delete build directory

--- build/third-party/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/olesaandreeva/Desktop/data_base/third-party
-# Build directory: /Users/olesaandreeva/Desktop/data_base/build/third-party
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
